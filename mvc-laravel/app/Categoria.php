8c65e9c46b1e4e8d7183bb825673e00113c71e20
Edneia Ribeiro da Silva <edsilva@saude.sp.gov.br>
2021-10-28 15:28:51 -0300

    Acao de Salvar

--- mvc-laravel/app/Categoria.php
+++ mvc-laravel/app/Categoria.php
@@ -9,4 +9,18 @@ class Categoria extends Model
     protected $fillable = [
         'nome'
     ];
+
+    public function salvar($dados)
+    {
+        /** Metodo utilizando o save, campo a campo */
+        //$this->nome = 'Nome:'.$dados['nome'];
+        //return $this->save();
+
+        /** Metodo utilizando o create, salva todos os campos fillable, mas ainda podemos tratar algum campo ou campos especificos */
+
+        //$dados['nome'] = 'Nome: '.$dados['nome'];
+
+        return $this->create($dados);
+
+    }
 }
--- mvc-laravel/app/Http/Controllers/Admin/CategoriaController.php
+++ mvc-laravel/app/Http/Controllers/Admin/CategoriaController.php
@@ -5,25 +5,53 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Categoria;
+
 class CategoriaController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        return view('admin.categorias.index');
     }
 
     public function criar()
     {
-        return view('admin.criar');
+        return view('admin.categorias.criar');
     }
 
     public function salvar(Request $request)
     {
-        dd($request->all());
+        //dd($request->all());
+
+        /* Exemplo 1 - para salvar a categoria pelo metodo simples, campo a campo */
+    
+        //$categoria = new Categoria;
+        //$categoria->nome = $dados['nome'];
+        //$categoria->save(); 
+        
+
+        /* Exemplo 2 - para salvar a categoria utilizando os campos fillable da classe Categoria  *
+           Interessante utilizar quando temos muitos campos para salvar para nao ter que fazer um por um
+           Para funcionar precisa configurar todos os campos na classe model Categoria e o array ($request) precisa enviar todos os campos configurados
+        */
+
+        //$dados = $request->all();
+
+        //Categoria::create($dados);
+
+        /* Exemplo 3 - para salvar a categoria colocando todas as regras/tratamentos na classe model Categoria */
+        $dados = $request->all();
+
+        $categoria = new Categoria;
+
+        $categoria->salvar($dados);
+
+        dd(Categoria::all());
+
     }
 
     public function editar($id)
     {
-        return view('admin.editar', compact('id'));
+        return view('admin.categorias.editar', compact('id'));
     }
 }
